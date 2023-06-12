d0018c2ccd5cbbaf4e027c49c4c3136aef9a3f5a
Andrea Amorosi <dreamorosi@gmail.com>
2023-06-12 19:06:59 +0200

    chore: clean package

--- .github/scripts/release_patch_package_json.js
+++ .github/scripts/release_patch_package_json.js
@@ -1,63 +1,29 @@
-const { readFileSync, writeFileSync } = require('node:fs');
-
-const outDir = './lib';
-const alphaPackages = [
-  '@aws-lambda-powertools/idempotency',
-];
-const betaPackages = [
-  '@aws-lambda-powertools/parameters',
-];
-
 /**
- * This script is used to create a new package.json file for the tarball that will be published to npm.
- * 
- * The original package.json file is read and the following fields are extracted:
- * - name
- * - version
- * - description
- * - author
- * - license
- * - homepage
- * - repository
- * - bugs
- * - keywords
- * - dependencies
- * - exports
- * - typeVersions
- * - main
- * - types
- * - files
- * 
- * For alplha & beta packages, the version number is updated to include the appropriate suffix.
- * 
- * The new package.json file is written to the package folder so that it can be packed and published to npm.
- * For alpha & beta packages, the package.json file is also temporarily updated with the new version number
- * so that the version number in the registry is correct and matches the tarball.
+ * This script is used to update the package.json file of an utility to include pre-release suffixes.
+ *
+ * We read the original package.json file and extract the name and version fields. Then, if the is an
+ * alpha or beta package, we update the version number to include a suffix. Finally, we write the updated
+ * package.json file to disk. The file will be restored to its original state after the release is complete.
  */
+const { readFileSync, writeFileSync } = require('node:fs');
+const { join, resolve } = require('node:path');
+
+if (process.argv.length < 3) {
+  console.error('Usage: node release_patch_package_json.js <package_path>\n');
+  process.exit(1);
+}
+const basePath = resolve(process.argv[2]);
+const packageJsonPath = join(basePath, 'package.json');
+const alphaPackages = ['@aws-lambda-powertools/idempotency'];
+const betaPackages = ['@aws-lambda-powertools/parameters'];
+
 (() => {
   try {
-    // Read the original package.json file
-    const pkgJson = JSON.parse(readFileSync('./package.json', 'utf8'))
-    // Extract the fields we want to keep
-    const {
-      name,
-      version: originalVersion,
-      description,
-      author,
-      license,
-      homepage,
-      repository,
-      bugs,
-      keywords,
-      dependencies,
-      exports,
-      typeVersions,
-      main,
-      types,
-      files,
-    } = pkgJson;
-
+    // Read the original package.json file & extract the name and version fields
+    const pkgJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
+    const { name, version: originalVersion } = pkgJson;
     let version = originalVersion;
+
     // If the package is an alpha or beta package, update the version number to include a suffix
     if (alphaPackages.includes(name)) {
       version = `${version}-alpha`;
@@ -65,33 +31,19 @@ const betaPackages = [
       version = `${version}-beta`;
     }
 
-    // Create a new package.json file with the updated version for the tarball
-    const newPkgJson = {
-      name,
-      version,
-      description,
-      author,
-      license,
-      homepage,
-      repository,
-      bugs,
-      keywords,
-      dependencies,
-      main,
-      types,
-      files,
-    };
-    // Not all packages have these fields yet, so only add them if they exist
-    if (exports) {
-      newPkgJson.exports = exports;
-    }
-    if (typeVersions) {
-      newPkgJson.typeVersions = typeVersions;
-    }
-
     // This version number will be picked up during the `npm publish` step, so that
     // it contains the correct version number and fields for the tarball.
-    writeFileSync('package.json', JSON.stringify(newPkgJson, null, 2));
+    writeFileSync(
+      packageJsonPath,
+      JSON.stringify(
+        {
+          ...pkgJson,
+          version,
+        },
+        null,
+        2
+      )
+    );
   } catch (err) {
     throw err;
   }
